2eba9f2f7565ceaa208d28c0f2da3988f7af0743
Mayuri Kambli <kamblimayuri@gmail.com>
2018-03-05 20:42:20 -0700

    BitMapFile, BitMapHeaderPage, ValueClass added

---
+++ src/main/java/bitmap/BitMapFile.java
@@ -0,0 +1,47 @@
+package bitmap;
+import global.*;
+
+public class BitMapFile {
+    BitMapFile(String filename){
+
+    }
+
+    /* Commented because of dependency on Columnarfile class
+    BitMapFile(java.lang.String filename, Columnarfile columnfile,
+               int ColumnNo, ValueClass value){
+
+    }
+    */
+
+    //Close the BitMap file
+    void close(){
+
+    }
+
+    //Destroy the entire BitMap file
+    void destroyBitMapFile(){
+
+    }
+
+    //Access method to data member
+
+    /*
+    BitMapHeaderPage getHeaderPage(){
+
+    }
+
+    //set the entry at the given position to 0.
+    boolean Delete(int position){
+
+    }
+
+    //set the entry at the given position to 1.
+    boolean Insert(int position){
+
+    }*/
+
+
+
+
+
+}
---
+++ src/main/java/global/ValueClass.java
@@ -0,0 +1,19 @@
+package global;
+
+/*Value Class
+Dependencies:
+1.ColumnarFile
+a. ValueClass getValue(TID tid, column)
+Read the value with the given column and tid from the columnar file.
+b. boolean createBitMapIndex(int columnNo, ValueClass value)
+If it doesn't exist, create a bitmap index for the given column and value.
+
+2. BitMapFile
+Constructor Parameter - An index file with given file name should not already exist; this creates the BitMapFile from scratch.
+*/
+
+public abstract class ValueClass {
+    ValueClass(){
+
+    }
+}
