1ca1adc7867e1c1b0b6bcb36796e8d1799ea143b
lbachani <lbachani@asu.edu>
2018-03-21 13:16:45 -0700

    Btree and Bitmap Initial Files

---
+++ src/main/java/iterator/BitmapScan.java
@@ -0,0 +1,20 @@
+package iterator;
+
+import heap.Tuple;
+
+import java.io.IOException;
+
+public class BitmapScan extends Iterator {
+
+    public Tuple getNext() throws Exception {
+
+
+        return null;
+    }
+
+    @Override
+    public void close() throws IOException {
+        // TODO Auto-generated method stub
+
+    }
+}
---
+++ src/main/java/iterator/BtreeScan.java
@@ -0,0 +1,20 @@
+package iterator;
+
+import heap.Tuple;
+
+import java.io.IOException;
+
+public class BtreeScan extends Iterator{
+
+    public Tuple getNext() throws Exception {
+
+
+        return null;
+    }
+
+    @Override
+    public void close() throws IOException {
+        // TODO Auto-generated method stub
+
+    }
+}
